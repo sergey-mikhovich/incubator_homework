cc4a226b101aaa62b9189f0706f46904851d5209
msi-a12uc <msi-a12uc@example.com>
2025-04-21 12:51:26 +0800

    implementation of HW5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -12,6 +12,9 @@ type PropsType = {
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
+
+    const activeClass = ({isActive}: {isActive: boolean}) => (isActive ? s.active : "")
+
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -31,7 +34,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={activeClass} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +42,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={activeClass} // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,7 +50,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={activeClass} // делает студент
                     >
                         Junior Plus
                     </NavLink>
